c1e7381fb4721ac69175203dad8250f24fddcb5e
DevNifemi <abioseimran@gmail.com>
2026-02-09 10:45:33 +0100

    set ALLOWED_MIME to null instead of a restricted set

--- clientConfig.js
+++ clientConfig.js
@@ -1,19 +1,6 @@
 export const CONFIG = {
   MAX_FILE_BYTES: 10 * 1024 * 1024,
-  ALLOWED_MIME: new Set([
-    "image/jpeg",
-    "image/png",
-    "image/webp",
-    "application/pdf",
-    "text/plain",
-    "text/markdown",
-    "text/csv",
-    // videos
-    "video/mp4",
-    "video/webm",
-    "video/ogg",
-    "video/quicktime", // .mov
-  ]),
+  ALLOWED_MIME: null, // accept all file types
   MAX_CHATS: 15,
   POSTHOG_KEY: "phc_2qafhOCTgCnygXsPEHOA0RBtJf5nvVsi7yIene4DWaF",
   POSTHOG_HOST: "https://us.i.posthog.com",
