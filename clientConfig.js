7e03db76cf345c678eee6d6b8d26bd9d11243210
DevNifemi <abioseimran@gmail.com>
2026-03-08 13:58:07 +0100

    add stripe publishable key to client config

--- clientConfig.js
+++ clientConfig.js
@@ -4,4 +4,6 @@ export const CONFIG = {
   MAX_CHATS: 15,
   POSTHOG_KEY: "phc_2qafhOCTgCnygXsPEHOA0RBtJf5nvVsi7yIene4DWaF",
   POSTHOG_HOST: "https://us.i.posthog.com",
+  STRIPE_PUBLISHABLE_KEY:
+    "pk_live_51Ra1PbGSPUS6dmsqKfWsAv5X5Y3QTU9AKtAOhUXItHHPfJ3LK5vNCex2eYy2uZQpvdOSeqA7OSvplV3tLt18kDj400qcIhc1Hv",
 };
